b3d134aca1084e8bfb5f7c10a27423aedc413803
Jose Riguera <jriguera@gmail.com>
2017-04-14 16:55:17 +0200

    improve scripts

--- config/bucket.sh
+++ config/bucket.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
 # Create the bucket for blobs and setup public read access
-BUCKET=diamond-boshrelease
+BUCKET=$(cat final.yml | awk '/bucket_name:/{ print $2 }')
 
 s3cmd mb "s3://${BUCKET}"
 s3cmd setacl "s3://${BUCKET}"  --acl-public --recursive
+
